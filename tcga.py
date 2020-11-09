f5c8ffa437abbf5709de3350a51b311f54f928bc
2101975298@qq.com <hzl1216>
2020-11-09 20:52:29 +0800

    update

--- tcga.py
+++ tcga.py
@@ -7,7 +7,7 @@ import numpy as np
 from util.dataset import get_datasets,RandomErasing,GaussianNoise,ToTensor
 from set_args import create_parser
 from util.net import ResNet50,TCN
-def main():
+def main(dataset):
     def create_model(ema=False):
         print("=> creating {ema}model ".format(
             ema='EMA ' if ema else ''))
@@ -50,10 +50,10 @@ def main():
     if args.val_size > 0:
         val_loader = data.DataLoader(val_set, batch_size=args.batch_size*args.unsup_ratio, shuffle=False, num_workers=args.num_workers)
     test_loader = data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+
     model = create_model()
     ema_model = create_model(ema=True)
-    tmp_model= create_model(ema=True)
-
+    tmp_model = create_model(ema=True)
     criterion = nn.CrossEntropyLoss().cuda()
     if args.optimizer == 'Adam':
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
@@ -67,11 +67,9 @@ def main():
         scheduler =  WarmupCosineSchedule(optimizer,warmup_step,totals)
     else:
         scheduler = None
-    all_labels = np.zeros([len(train_unlabeled_set), 33])
+    all_labels = np.zeros([len(train_unlabeled_set), 10])
     # optionally resume from a checkpoint
-    title = 'tcga'
-    best_acc = 0
-    best_epoch = 0
+    title = dataset
     if args.resume:
         assert os.path.isfile(args.resume), "=> no checkpoint found at '{}'".format(args.resume)
         print("=> loading checkpoint '{}'".format(args.resume))
@@ -81,76 +79,69 @@ def main():
         ema_model.load_state_dict(checkpoint['ema_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         print("Evaluating the  model:")
-        if args.val_size > 0:
-            val_loss, val_acc = validate(val_loader, model, criterion)
+        if args.val_size>0:
+            val_loss, val_acc = validate(val_loader, model, criterion,args.start_epoch)
         else:
             val_loss, val_acc = 0, 0
-        test_loss, test_acc = validate(test_loader, model, criterion)
+        test_loss, test_acc = validate(test_loader, model, criterion, args.start_epoch)
         print("=> loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint['epoch']))
 
-        logger = Logger(os.path.join(args.out_path, '%s_log_%d_%d.txt'%(title,args.n_labeled,args.index)), title=title, resume=True)
+        logger = Logger(os.path.join(args.out_path, '%s_log_%d.txt'%(dataset,args.n_labeled)), title=title, resume=True)
         logger.append([args.start_epoch, 0, 0, val_loss, val_acc,test_loss, test_acc])
     else:
-        logger = Logger(os.path.join(args.out_path, '%s_log_%d_%d.txt'%(title,args.n_labeled,args.index)), title=title)
-        logger.set_names(['epoch', 'Train_class_loss',  'Train_consistency_loss', 'Val_Loss', 'Val_Acc.', 'Test_Loss', 'Test_Acc'])
+        logger = Logger(os.path.join(args.out_path, '%s_log_%d.txt'%(dataset,args.n_labeled)), title=title)
+        logger.set_names(['epoch', 'Train_class_loss',  'Train_consistency_loss', 'Val_Loss', 'Val_Acc.', 'Test_Loss', 'Test_Acc.'])
 
     for epoch in range(args.start_epoch, args.epochs):
         start_time = time.time()
         # train for one epoch
-        
+        class_loss, cons_loss,all_labels = train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model,optimizer, ema_optimizer,all_labels, epoch, scheduler)
         if epoch >= args.ema_stage:
-            print('train in semi-supervised stage2')
-            all_labels = get_u_label(ema_model, train_unlabeled_loader2, all_labels)
-            class_loss, cons_loss = train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model,optimizer,ema_optimizer, all_labels, epoch, criterion,scheduler)
-        else:
-            print(' train in semi-supervised stage1')
-            cons_loss = 0
-            class_loss = train(train_labeled_loader, model, ema_model,optimizer, ema_optimizer, epoch, criterion, scheduler)
+            all_labels = get_u_label(ema_model, train_unlabeled_loader2,all_labels)
+
         print("--- training epoch in %s seconds ---" % (time.time() - start_time))
 
         if args.evaluation_epochs and (epoch + 1) % args.evaluation_epochs == 0:
             start_time = time.time()
             print("Evaluating the  model:")
             if args.val_size>0:
-                val_loss, val_acc = validate(val_loader, model, criterion)
+                val_loss, val_acc = validate(val_loader, model, criterion,epoch)
             else:
                 val_loss, val_acc = 0, 0
-            test_loss, test_acc = validate(test_loader, model, criterion)
+            test_loss, test_acc = validate(test_loader, model, criterion, epoch)
             print("--- validation in %s seconds ---" % (time.time() - start_time))
             logger.append([epoch, class_loss, cons_loss, val_loss, val_acc,test_loss, test_acc])
-            if test_acc > best_acc:
-                best_acc = test_acc
-                best_epoch=epoch
-            if epoch > best_epoch+100:
-                break
+
             print("Evaluating the EMA model:")
             if args.val_size > 0:
-                ema_val_loss, ema_val_acc = validate(val_loader, ema_model, criterion)
+                ema_val_loss, ema_val_acc = validate(val_loader, ema_model, criterion,epoch)
             else:
                 ema_val_loss, ema_val_acc = 0, 0
-            ema_test_loss, ema_test_acc = validate(test_loader, ema_model, criterion)
+            ema_test_loss, ema_test_acc = validate(test_loader, ema_model, criterion, epoch)
             print("--- validation in %s seconds ---" % (time.time() - start_time))
             logger.append([epoch, class_loss, cons_loss, ema_val_loss, ema_val_acc,ema_test_loss, ema_test_acc])
 
-    save_checkpoint(
-                '%s_%d'%(title, args.n_labeled),
+        if args.checkpoint_epochs and (epoch + 1) % args.checkpoint_epochs == 0:
+            save_checkpoint(
+                '%s_%d'%(dataset, args.n_labeled),
                 {
                 'epoch': epoch + 1,
                 'state_dict': model.state_dict(),
                 'ema_state_dict': ema_model.state_dict(),
                 'optimizer' : optimizer.state_dict(),
-            }, 'checkpoint_path', args.index)
+            }, 'checkpoint_path', epoch + 1)
 
 if __name__ == '__main__':
-    dirs = ['result', 'data', 'checkpoint_path']
+    dirs = [ 'result','data','checkpoint_path']
     for path in dirs:
         if os.path.exists(path) is False:
-            os.makedirs(path) 
+            os.makedirs(path)
     args = create_parser()
-    print('train in %d fold data'%args.index)
-#    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed) 
-    torch.cuda.manual_seed(args.seed)
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    if args.seed is None:
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
     set_args(args)
-    main()
+    main('tcga')
+
--- train_tool.py
+++ train_tool.py
@@ -1,7 +1,6 @@
 import time
 import os
 
-from util import ramps
 import torch.nn as nn
 import torch
 import logging
@@ -9,8 +8,7 @@ from util.utils import *
 import math
 from torch.optim.lr_scheduler import LambdaLR
 import torch.nn.functional as F
-from sklearn.metrics import confusion_matrix
-from util.losses import FocalLoss
+
 LOG = logging.getLogger('main')
 args = None
 
@@ -20,7 +18,8 @@ def set_args(input_args):
     args = input_args
 
 
-def train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model,optimizer, ema_optimizer, all_labels, epoch, criterion, scheduler=None):
+def train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model, optimizer, ema_optimizer, all_labels,
+               epoch, scheduler=None):
     labeled_train_iter = iter(train_labeled_loader)
     unlabeled_train_iter = iter(train_unlabeled_loader)
 
@@ -38,47 +37,56 @@ def train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model,op
             inputs_x, targets_x, label_index = labeled_train_iter.next()
 
         try:
-            (inputs_u1, inputs_u2), _, unlabel_index = unlabeled_train_iter.next()
+            (inputs_aug, inputs_std), _, unlabel_index = unlabeled_train_iter.next()
         except:
             unlabeled_train_iter = iter(train_unlabeled_loader)
-            (inputs_u1, inputs_u2), _, unlabel_index = unlabeled_train_iter.next()
+            (inputs_aug, inputs_std), _, unlabel_index = unlabeled_train_iter.next()
 
         # measure data loading time
         meters.update('data_time', time.time() - end)
         inputs_x = inputs_x.cuda()
-        inputs_u1 = inputs_u1.cuda()
-        inputs_u2 = inputs_u2.cuda()
-        all_inputs = torch.cat([inputs_u1, inputs_u2], dim=0)
+        inputs_aug = inputs_aug.cuda()
+        inputs_std = inputs_std.cuda()
 
         batch_size = inputs_x.size(0)
-        targets_x = torch.zeros(batch_size, 33).scatter_(1, targets_x.view(-1, 1), 1).cuda(non_blocking=True)
+        targets_x = torch.zeros(batch_size, 10).scatter_(1, targets_x.view(-1, 1), 1).cuda(non_blocking=True)
 
-
-        targets_u = torch.FloatTensor(all_labels[unlabel_index,:]).cuda()
+        if epoch <= args.ema_stage:
+            with torch.no_grad():
+                logits_aug = model(inputs_aug)
+                logits_std = model(inputs_std)
+                targets_u = (torch.softmax(logits_aug, dim=1) + torch.softmax(logits_std, dim=1)) / 2
+        else:
+            targets_u = torch.FloatTensor(all_labels[unlabel_index, :]).cuda()
         targets_u = sharpen(targets_u)
         targets_u = targets_u.detach()
 
-
-        length = get_unsup_size(epoch+i / args.epoch_iteration)
-        input_a = torch.cat([inputs_x, inputs_u1[: length]], dim=0)
-        target_a = torch.cat([targets_x, targets_u[: length]], dim=0)
-        l = np.random.beta(args.alpha, args.alpha)
-        l = max(l,1-l)
-        idx = torch.randperm(input_a.size(0))
-        input_b = input_a[idx]
-        target_b = target_a[idx]
-        mixed_inputs = l * input_a + (1 - l) * input_b
-        mixed_targets = l * target_a + (1 - l) * target_b
-        del input_a,target_a,input_b,target_b
-        all_inputs = torch.cat([all_inputs, mixed_inputs], dim=0)
-        logits = model(all_inputs)
-        outputs_u1, outputs_u2 = logits[:args.batch_size * args.unsup_ratio * 2].chunk(2)
-        outputs_mixup = logits[args.batch_size * args.unsup_ratio * 2:]
-        del logits
-        loss, class_loss, consistency_loss = semiloss_mixup(outputs_mixup,mixed_targets, outputs_u2, outputs_u1.detach(), epoch)
-
-
-
+        all_inputs = torch.cat([inputs_aug, inputs_std], dim=0)
+        if args.mixup:
+            length = get_unsup_size(epoch + i / args.epoch_iteration)
+            input_a = torch.cat([inputs_x, inputs_std[: length]], dim=0)
+            target_a = torch.cat([targets_x, targets_u[: length]], dim=0)
+            l = np.random.beta(args.alpha, args.alpha)
+            l = max(l, 1 - l)
+            idx = torch.randperm(input_a.size(0))
+            input_b = input_a[idx]
+            target_b = target_a[idx]
+            mixed_inputs = l * input_a + (1 - l) * input_b
+            mixed_targets = l * target_a + (1 - l) * target_b
+            del input_a, target_a, input_b, target_b
+            all_inputs = torch.cat([all_inputs, mixed_inputs], dim=0)
+            all_logits = model(all_inputs)
+            logits_aug, logits_std = all_logits[:args.batch_size * args.unsup_ratio * 2].chunk(2)
+            if args.softmax_temp > 0:
+                logits_std = logits_std / args.softmax_temp
+            logits_mixup = all_logits[args.batch_size * args.unsup_ratio * 2:]
+            del all_logits
+            loss, class_loss, consistency_loss = semiloss_mixup(logits_mixup, mixed_targets, logits_aug,
+                                                                logits_std.detach())
+        else:
+            logits_x = model(inputs_x)
+            logits_u = model(inputs_aug)
+            loss, class_loss, consistency_loss = semiloss(logits_x, targets_x, logits_aug, logits_std.detach())
         meters.update('loss', loss.item())
         meters.update('class_loss', class_loss.item())
         meters.update('cons_loss', consistency_loss.item())
@@ -101,83 +109,11 @@ def train_semi(train_labeled_loader, train_unlabeled_loader, model, ema_model,op
                 'Class {meters[class_loss]:.4f}\t'
                 'Cons {meters[cons_loss]:.4f}\t'.format(
                     epoch, i, args.epoch_iteration, meters=meters))
+    ema_optimizer.step(True)
+    return meters.averages()['class_loss/avg'], meters.averages()['cons_loss/avg'], all_labels
 
 
-    ema_optimizer.step(bn=True)
-    return meters.averages()['class_loss/avg'], meters.averages()['cons_loss/avg']
-
-def train(train_labeled_loader, model, ema_model,optimizer, ema_optimizer,epoch,criterion, scheduler=None):
-    labeled_train_iter = iter(train_labeled_loader)
-
-
-    meters = AverageMeterSet()
-
-    # switch to train mode
-    model.train()
-    ema_model.train()
-    end = time.time()
-    for i in range(args.epoch_iteration):
-        try:
-            inputs_x, targets_x, label_index = labeled_train_iter.next()
-        except:
-            labeled_train_iter = iter(train_labeled_loader)
-            inputs_x, targets_x, label_index = labeled_train_iter.next()
-
-
-
-        # measure data loading time
-        meters.update('data_time', time.time() - end)
-        inputs_x = inputs_x.cuda()
-
-        batch_size = inputs_x.size(0)
-        targets_x = torch.zeros(batch_size, 33).scatter_(1, targets_x.view(-1, 1), 1).cuda(non_blocking=True)
-
-
-
-        if args.mixup:
-            l = np.random.beta(args.alpha, args.alpha)
-            idx = torch.randperm(targets_x.size(0))
-            input_a, input_b = inputs_x, inputs_x[idx]
-            target_a, target_b = targets_x, targets_x[idx]
-
-            mixed_input = l * input_a + (1 - l) * input_b
-            mixed_target = l * target_a + (1 - l) * target_b
-
-            outputs = model(mixed_input)
-            
-#            loss_mask = torch.max(outputs,dim=1)[0].gt(args.tsa).float().detach()
-            loss = -torch.mean(torch.sum(F.log_softmax(outputs, dim=1) * mixed_target, dim=1))
-#            loss = criterion(outputs,mixed_target)
-        else:
-            outputs = model(inputs_x)
-            loss, class_loss, consistency_loss = criterion(outputs, outputs)
-        meters.update('loss', loss.item())
-
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        ema_optimizer.step()
-        if scheduler is not None:
-            scheduler.step()
-        # measure elapsed time
-        meters.update('batch_time', time.time() - end)
-        end = time.time()
-
-        if i % args.print_freq == 0:
-            print(
-                'Epoch: [{0}][{1}/{2}]\t'
-                'Time {meters[batch_time]:.3f}\t'
-                'Data {meters[data_time]:.3f}\t'
-                'Class {meters[loss]:.4f}\t'.format(
-                    epoch, i, args.epoch_iteration, meters=meters))
-
-
-    ema_optimizer.step(bn=True)
-    return meters.averages()['loss/avg']
-
-
-def validate(val_loader, model, criterion):
+def validate(val_loader, model, criterion, epoch):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
@@ -186,27 +122,29 @@ def validate(val_loader, model, criterion):
 
     # switch to evaluate mode
     model.eval()
+
     end = time.time()
     all_labels = None
-    all_outputs = None
+    all_logits = None
     with torch.no_grad():
         for batch_idx, (inputs, targets, _) in enumerate(val_loader):
             # measure data loading time
             data_time.update(time.time() - end)
             inputs, targets = inputs.cuda(), targets.cuda(non_blocking=True)
+
             # compute output
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
+            logits = model(inputs)
+            loss = criterion(logits, targets)
 
             if all_labels is None:
                 all_labels = targets
-                all_outputs = outputs
+                all_logits = logits
             else:
                 all_labels = torch.cat([all_labels, targets], dim=0)
-                all_outputs = torch.cat([all_outputs, outputs], dim=0)
+                all_logits = torch.cat([all_logits, logits], dim=0)
 
             # measure accuracy and record loss
-            prec1, prec5 = accuracy(outputs, targets, topk=(1, 5))
+            prec1, prec5 = accuracy(logits, targets, topk=(1, 5))
 
             losses.update(loss.item(), inputs.size(0))
             top1.update(prec1.item(), inputs.size(0))
@@ -217,16 +155,16 @@ def validate(val_loader, model, criterion):
             end = time.time()
 
             # plot progress
-    print(
-        ' Data: {data:.3f}s | Batch: {bt:.3f}s | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
-            data=data_time.avg,
-            bt=batch_time.avg,
-            loss=losses.avg,
-            top1=top1.avg,
-            top5=top5.avg,
-        ))
-#    conf_matrix = confusion_matrix(all_outputs, all_labels)
-#    plot_confusion_matrix(conf_matrix.numpy(), epoch)
+
+        print(
+            'Data: {data:.3f}s | Batch: {bt:.3f}s | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                data=data_time.avg,
+                bt=batch_time.avg,
+                loss=losses.avg,
+                top1=top1.avg,
+                top5=top5.avg,
+            ))
+
     return losses.avg, top1.avg
 
 
@@ -260,29 +198,18 @@ class WeightEMA(object):
                 if args.optimizer == 'Adam':
                     param.data.mul_(1 - self.wd)
 
-def save_checkpoint(name ,state, dirpath, epoch):
+
+def save_checkpoint(name, state, dirpath, epoch):
     filename = '%s_%d.ckpt' % (name, epoch)
     checkpoint_path = os.path.join(dirpath, filename)
     torch.save(state, checkpoint_path)
     LOG.info("--- checkpoint saved to %s ---" % checkpoint_path)
 
 
-def get_current_consistency_weight(epoch):
-    # Consistency ramp-up from https://arxiv.org/abs/1610.02242
-    return args.consistency * ramps.sigmoid_rampup(epoch, args.consistency_rampup)
-
-
-def get_current_entropy_weight(epoch):
-    if epoch > args.consistency_rampup:
-        return args.entropy_cost
-    else:
-        return 0
-
-
-def sharpen(outputs):
-    outputs = outputs ** 2
-    outputs = outputs / outputs.sum(dim=1, keepdim=True)
-    return outputs
+def sharpen(logits):
+    logits = logits ** 2
+    logits = logits / logits.sum(dim=1, keepdim=True)
+    return logits
 
 
 class WarmupCosineSchedule(LambdaLR):
@@ -292,7 +219,7 @@ class WarmupCosineSchedule(LambdaLR):
         If `cycles` (default=0.5) is different from default, learning rate follows cosine function after warmup.
     """
 
-    def __init__(self, optimizer, warmup_steps, t_total,alpha=0.004, cycles=.5, last_epoch=-1):
+    def __init__(self, optimizer, warmup_steps, t_total, alpha=0.004, cycles=.5, last_epoch=-1):
         self.warmup_steps = warmup_steps
         self.t_total = t_total
         self.cycles = cycles
@@ -307,95 +234,70 @@ class WarmupCosineSchedule(LambdaLR):
         cosine_decay = 0.5 * (1. + math.cos(math.pi * float(self.cycles) * 2.0 * progress))
         decayed = (1 - self.alpha) * cosine_decay + self.alpha
         return decayed
-    
 
 
-def mixup(all_inputs, all_targets, batch_size, model,epoch):
+def mixup(all_inputs, all_targets, model, epoch):
     l = np.random.beta(args.alpha, args.alpha)
 
     length = get_unsup_size(epoch)
-    all_inputs = all_inputs[:args.batch_size+length]
-    all_targets = all_targets[:args.batch_size+length]
+    all_inputs = all_inputs[:args.batch_size + length]
+    all_targets = all_targets[:args.batch_size + length]
     idx = torch.randperm(all_inputs.size(0))
-    input_a, input_b = all_inputs, all_inputs[idx]
-    target_a, target_b = all_targets, all_targets[idx]
+    input_a, input_b = all_inputs[idx], all_inputs[idx][idx]
+    target_a, target_b = all_targets[idx], all_targets[idx][idx]
 
     mixed_input = l * input_a + (1 - l) * input_b
     mixed_target = l * target_a + (1 - l) * target_b
 
-    outputs = model(mixed_input)
-    
-    return outputs, mixed_target
+    logits = model(mixed_input)
+
+    return logits, mixed_target
 
 
-def semiloss(outputs_x, targets_x, outputs_u, targets_u,epoch):
-    class_loss = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
-    consistency_loss = torch.mean(torch.sum(F.softmax(targets_u,1) * (F.log_softmax(targets_u, 1) - F.log_softmax(outputs_u, dim=1)), 1))
+def semiloss(logits_x, targets_x, logits_u, targets_u):
+    class_loss = -torch.mean(torch.sum(F.log_softmax(logits_x, dim=1) * targets_x, dim=1))
+    consistency_loss = torch.mean(
+        torch.sum(F.softmax(targets_u, 1) * (F.log_softmax(targets_u, 1) - F.log_softmax(logits_u, dim=1)), 1))
 
-    return class_loss + args.consistency_weight*consistency_loss, class_loss, consistency_loss
+    return class_loss + args.consistency_weight * consistency_loss, class_loss, consistency_loss
 
 
-def semiloss_mixup(outputs_x, targets_x, outputs_u, targets_u, epoch):
-    probs_u = torch.softmax(targets_u, dim=1)
-    class_loss = -torch.mean(torch.sum(F.log_softmax(outputs_x, dim=1) * targets_x, dim=1))
+def semiloss_mixup(logits_x, targets_x, logits_u, targets_u):
+    class_loss = -torch.mean(torch.sum(F.log_softmax(logits_x, dim=1) * targets_x, dim=1))
     if args.confidence_thresh > 0:
-        loss_mask = torch.max(probs_u,dim=1)[0].gt(args.confidence_thresh).float().detach()
-        consistency_loss = -torch.sum(F.log_softmax(outputs_u, dim=1) * F.softmax(targets_u, 1), dim=1)
-        #consistency_loss = torch.sum(F.softmax(targets_u, 1) * (F.log_softmax(targets_u, 1) - F.log_softmax(outputs_u, dim=1)),1)
-        consistency_loss = torch.sum(consistency_loss*loss_mask)/torch.max(torch.sum(loss_mask), torch.tensor(1.))
+        loss_mask = torch.max(torch.softmax(targets_u, dim=1), dim=1)[0].gt(args.confidence_thresh).float().detach()
+        consistency_loss = torch.mean(
+            torch.sum(F.softmax(targets_u, 1) * (F.log_softmax(targets_u, 1) - F.log_softmax(logits_u, dim=1)),
+                      1) * loss_mask)
     else:
-        consistency_loss = -torch.mean(torch.sum(F.log_softmax(outputs_u, dim=1) * F.softmax(targets_u, 1), dim=1))
-    if args.entropy_cost >0:
-        entropy_loss = -args.entropy_cost * torch.mean(torch.sum(torch.mul(F.softmax(outputs_u,dim=1), F.log_softmax(outputs_u,dim=1)),dim=1))
+        consistency_loss = torch.mean(
+            torch.sum(F.softmax(targets_u, 1) * (F.log_softmax(targets_u, 1) - F.log_softmax(logits_u, dim=1)), 1))
+
+    if args.entropy_cost > 0:
+        entropy_loss = - torch.mean(
+            torch.sum(torch.mul(F.softmax(logits_u, dim=1), F.log_softmax(logits_u, dim=1)), dim=1))
     else:
         entropy_loss = 0
-    return class_loss + args.consistency_weight * consistency_loss + entropy_loss, class_loss, consistency_loss
+    return class_loss + args.consistency_weight * consistency_loss + args.entropy_cost * entropy_loss, class_loss, consistency_loss
 
 
-def interleave_offsets(batch, nu):
-    groups = [batch // (nu + 1)] * (nu + 1)
-    for x in range(batch - sum(groups)):
-        groups[-x - 1] += 1
-    offsets = [0]
-    for g in groups:
-        offsets.append(offsets[-1] + g)
-    assert offsets[-1] == batch
-    return offsets
-
-
-def interleave(xy, batch):
-    nu = len(xy) - 1
-    offsets = interleave_offsets(batch, nu)
-    xy = [[v[offsets[p]:offsets[p + 1]] for p in range(nu + 1)] for v in xy]
-    for i in range(1, nu + 1):
-        xy[0][i], xy[i][i] = xy[i][i], xy[0][i]
-    return [torch.cat(v, dim=0) for v in xy]
-
-
-def confusion_matrix(preds, labels, n_class=33):
-    conf_matrix = torch.zeros(n_class, n_class)
-    preds = torch.argmax(preds, 1)
-    for p, t in zip(preds, labels):
-        conf_matrix[p, t] += 1
-    return conf_matrix
-
-
-def get_u_label(model, loader,all_labels):
+def get_u_label(model, loader, all_labels):
     model.eval()
     with torch.no_grad():
         for batch_idx, (inputs, _, index) in enumerate(loader):
             inputs = inputs.cuda()
 
             # compute output
-            outputs = model(inputs)
-            all_labels[index] = torch.softmax(outputs,dim=1).cpu().numpy()
+            logits = model(inputs)
+            all_labels[index] = torch.softmax(logits, dim=1).cpu().numpy()
 
     return all_labels
 
-def scheduler(epoch,totals=None,start=0.0,end=1.0):
+
+def scheduler(epoch, totals=None, start=0.0, end=1.0):
     if totals is None:
         totals = args.epochs
-    step_ratio = epoch/totals
+    step_ratio = epoch / totals
     if args.scheduler == 'linear':
         coeff = step_ratio
     elif args.scheduler == 'exp':
@@ -404,8 +306,9 @@ def scheduler(epoch,totals=None,start=0.0,end=1.0):
         coeff = 1 - np.exp((-step_ratio) * 5)
     else:
         return 1.0
-    return coeff * (end - start) +start
+    return coeff * (end - start) + start
+
 
 def get_unsup_size(epoch):
-    size = int(args.mixup_size*args.batch_size*scheduler(epoch))
+    size = int(min(args.mixup_size, args.unsup_ratio) * args.batch_size * scheduler(epoch))
     return size
