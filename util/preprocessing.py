a6c927e6a13d1353f117452ec0d5fca3eefbba0d
2101975298@qq.com <hzl1216>
2020-11-10 20:12:54 +0800

    update

--- util/preprocessing.py
+++ util/preprocessing.py
@@ -86,7 +86,41 @@ def merge_geo_tcga(tcga_data, geo_data, annotation_path):
     features_g = scaler.transform(features_g)
     pd.DataFrame(features_g, columns=feature_name).to_csv('data/geo_data.csv', index=0)
 
-def normalise_and_save(features,feature_name,labels,scaler=None,file_path='train.csv'):
+
+def split_geo(geo_data, annotation_path):
+    geo_table = pd.read_csv(geo_data)
+    feature_name_g = list(geo_table)[1:]
+    annotation = pd.read_csv(annotation_path, dtype=str)
+    gene_id_annotation = list(annotation.loc[:, "gene"])
+    labels = np.array(geo_table.iloc[:, 0])
+    idx_g = []
+    print('features have been sorted based on chromosome')
+    for gene_id in gene_id_annotation:
+        if gene_id in feature_name_g:
+            idx_g.append(feature_name_g.index(gene_id))
+
+    features_g = np.array(geo_table.iloc[:, 1:])
+    features_g = features_g[:, idx_g]
+    features = feature_name_g[idx_g]
+    # numpy is different from lis
+
+    print('remove the features that  Variance is low than threshold')
+    selector = VarianceThreshold(threshold=1)
+    selector.fit(feature_name_g)
+    idx2 = selector.get_support(indices=True)
+    features_g = features_g[:, idx2]
+    feature_name = np.array(features)[idx2]
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=13)
+    scaler = preprocessing.MinMaxScaler().fit(features_g)
+
+    for index, (train_index, test_index) in enumerate(kf.split(features_g)):
+        x_train, x_test, y_train, y_test = features_g[train_index], features_g[test_index],labels[train_index], labels[test_index]
+        normalise_and_save(x_train, feature_name, y_train, scaler, 'data/train_%d.csv'%index)
+        normalise_and_save(x_test, feature_name, y_test, scaler, 'data/test_%d.csv'%index)
+
+
+def normalise_and_save(features, feature_name, labels, scaler=None, file_path='train.csv'):
     print(features.shape,len(feature_name))
     print('normalise the data in [0,1])')
 
@@ -105,28 +139,9 @@ def normalise_and_save(features,feature_name,labels,scaler=None,file_path='train
 
 
 if __name__ == '__main__':
-    # feature_selection_and_sort_by_chromosome('data/TCGA_data.csv','data/Annotation.csv')
-    merge_geo_tcga('data/big_gene_expression_data.csv', 'data/big_data_xiong.csv', 'data/Annotation.csv')
-
-    '''
-    big_table1 = pd.read_csv('D:/GEO数据/big_data_xiong.csv',nrows =5)
-    big_table2 = pd.read_csv('D:/TCGA-DATA/big_gene_expression_data.csv',nrows =5)
-    annotation = pd.read_csv('D:/TCGA-DATA/Annotation.csv', dtype=str)
-    gene_id_annotation = list(annotation.loc[:, "gene"])
-    print(gene_id_annotation)
-    feature_name1 = list(big_table1)
-    feature_name2 = list(big_table2)
-    feature_name2 = [feature.split('|')[0] for feature in feature_name2]
-    for gene_id in feature_name2:
-        if gene_id in gene_id_annotation:
-            print(gene_id)
-    print(feature_name2)
-    count = 0
-    for i in feature_name1:
-        if i in feature_name2:
-            count += 1
-    print(count)
-    '''
+# feature_selection_and_sort_by_chromosome('data/TCGA_data.csv','data/Annotation.csv')
+#   merge_geo_tcga('data/big_gene_expression_data.csv', 'data/big_data_xiong.csv', 'data/Annotation.csv')
+    split_geo( 'data/big_data_xiong.csv', 'data/Annotation.csv')
 
 
 
